5a3b92f742659a725ea33f9968f7a541a2a78837
kwang7 <kwang7@stuy.edu>
2017-01-12 08:42:45 -0500

    Character.java Plants.java Array.java

--- Array.java
+++ Array.java
@@ -1,9 +1,13 @@
+import cs1.Keyboard;
+
 public class Array {
 
     public static String[][] board = new String[9][9];
     
+
     public static String ArrayToString ( String [][] arr ) {
 	rows(board);
+	chars(board);
 	String retstr = "";
 	for ( int s = 0; s < arr.length ; s++ ) {
 	    for ( int x = 0 ; x < arr[s].length ; x ++) {
@@ -31,11 +35,23 @@ public class Array {
 	    String a = "" + s;
 	    arr[s][0] = a;
 	}
-   
+    }
+
+    public static void chars ( String [][] arr ) {
+	int x = Keyboard.readInt();
+	int y = Keyboard.readInt();
+	arr[y][x] = Keyboard.readWord();
     }
 
     public static void main ( String[] args ) {
-	//rows(board);
-	System.out.println ( ArrayToString ( board ) );
+	//	System.out.println ( ArrayToString ( board ) );
+	
+	String r = "";
+	Array kelly = new Array();
+	r = "Welcome to PvZ";
+	//kelly.chars(board);
+	System.out.println( kelly.ArrayToString(board) );
+	
+
     }
 }
