e417c444b097dfff32b6dfaadbec9c4a6d4b423c
DimitriyL <dleksanov@stuy.edu>
2017-01-22 14:05:13 -0500

    Updated Plants.java, no comments

--- Plants.java
+++ Plants.java
@@ -10,55 +10,14 @@ public class Plants extends Character{
 	freq = freq1;
 	super.xCor = xCor1;
 	super.yCor = yCor1;
-	if (type1 == "P") {
-        symbol = "P";
-        health = 10;
-        atk = 5;
-        cost = 5;
-    }
-    
-    if (type1 == "S") {
-        symbol = "S";
-        health = 6;
-        atk = 4;
-        cost = 3;
-    }
-    
-    if (type1 == "T") {
-        symbol = "T";
-        health = 25;
-        atk = 0;
-        cost = 10;
-    }
-    
-    if (type1 == "B") {
-        symbol = "B";
-        health = 10;
-        atk = 7;
-        cost = 8;
-    }
-    
-    if (type1 == "C") {
-        symbol = "C";
-        health = 8;
-        atk = 8;
-        cost = 7;
-    }
-    
-    if (type1 == "W") {
-        symbol = "W";
-        health = 15;
-        atk = 15;
-        cost = 20;
-    }
+	type = type1;
     }
 
     public String toString() {
-	return symbol;
+	return type;
     }
 
     public String shoot () {
 	return bullet;
     } 
-    
 }
