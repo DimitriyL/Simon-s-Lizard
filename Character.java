d6f870096ec9758a168dd4fed5d4cf66ad79af09
DimitriyL <dleksanov@stuy.edu>
2017-01-22 23:48:08 -0500

    Character.java final, with comments

--- Character.java
+++ Character.java
@@ -1,12 +1,22 @@
+/*
+this was a class created to be a superclass for Plants and Zombie
+however, it ultimately became easier to simply use those as separate classes instead
+ */
 public class Character {
 
     protected char symbol;
+    //replaced by "type" in Plants and Zombie
     protected int health;
-    protected int atk;
+    //replaced by "health"
     protected int xCor;
     protected int yCor;
-    protected String name;
+    //xCor and yCor ultimately went unused because of plaBoard and zomBoax
 
+    /*
+      int adjustHealth(int amount)
+      this method takes the pre-existing health of a character, alters it by input value "amount," and outputs the new health value
+      however, this was overriden in Zombie to output the old health
+     */
     public int adjustHealth(int amount){
 	health -= amount;
 	return health;
